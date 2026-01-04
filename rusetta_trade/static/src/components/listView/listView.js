9afc9b3b6f680dff95ada379ba4deedd31af3eab
mahmoudElshimi <mahmoudelshimi@protonmail.ch>
2026-01-04 19:58:20 +0200

    [Add] rusetta_trade: Adding dynamic lists for trades

--- rusetta_trade/static/src/components/listView/listView.js
+++ rusetta_trade/static/src/components/listView/listView.js
@@ -1,8 +1,24 @@
-import {Component} from "@odoo/owl";
+import {Component, useState} from "@odoo/owl";
 import {registry} from "@web/core/registry";
+import {useService} from "@web/core/utils/hooks";
 
 export class CustomList extends Component {
 	static template = "rusetta_trade.CustomList";
+
+	setup() {
+		this.state = useState(
+			{
+				"trades": [],
+			}
+		);
+		this.orm = useService("orm");
+		this.loadTrades();
+	};
+
+	async loadTrades() {
+		const trade = await this.orm.searchRead("rusetta.trade", [], []);
+		this.state.trades = trade;
+	};
 }
 
 registry.category("actions").add("rusetta_trade.action_custom_list", CustomList);
