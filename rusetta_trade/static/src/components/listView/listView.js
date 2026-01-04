d2365b3835896c6325e6790b23e14ccefd0a806d
mahmoudElshimi <mahmoudelshimi@protonmail.ch>
2026-01-04 20:02:14 +0200

    [IMP] rusetta_trade: Improve OWL JS implementation

--- rusetta_trade/static/src/components/listView/listView.js
+++ rusetta_trade/static/src/components/listView/listView.js
@@ -1,24 +1,26 @@
-import {Component, useState} from "@odoo/owl";
-import {registry} from "@web/core/registry";
-import {useService} from "@web/core/utils/hooks";
+import { Component, useState, onWillStart } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 
 export class CustomList extends Component {
-	static template = "rusetta_trade.CustomList";
+    static template = "rusetta_trade.CustomList";
 
-	setup() {
-		this.state = useState(
-			{
-				"trades": [],
-			}
-		);
-		this.orm = useService("orm");
-		this.loadTrades();
-	};
+    setup() {
+        this.state = useState({ trades: [] });
+        this.orm = useService("orm");
 
-	async loadTrades() {
-		const trade = await this.orm.searchRead("rusetta.trade", [], []);
-		this.state.trades = trade;
-	};
+        onWillStart(async () => {
+            this.state.trades = await this.orm.searchRead(
+                "rusetta.trade",
+                [],
+                ["create_date", "opt_type", "state"]
+            );
+        });
+    }
 }
 
-registry.category("actions").add("rusetta_trade.action_custom_list", CustomList);
+registry.category("actions").add(
+    "rusetta_trade.action_custom_list",
+    CustomList
+);
+
